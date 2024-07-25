00e42825f099c9e0a4b0911c5cf95db27126d335
Artem Vitae <15745003+artemii235@users.noreply.github.com>
2024-07-25 17:10:36 +0700

    Map different host dir depending on GITHUB_ACTIONS.

--- block_relayer/tests/integration.rs
+++ block_relayer/tests/integration.rs
@@ -3,6 +3,7 @@ use block_relayer_lib::run_init_program;
 use bollard::container::RemoveContainerOptions;
 use bollard::Docker;
 use once_cell::sync::Lazy;
+use std::env;
 use std::path::PathBuf;
 use std::process::{Child, Command, Stdio};
 use std::sync::Mutex;
@@ -46,6 +47,7 @@ static TEST_CTX: Lazy<TestCtx> = Lazy::new(|| {
 
     let anchor_localnet_handle = Command::new("anchor")
         .arg("localnet")
+        .arg("--skip-build")
         .current_dir(current_dir.join("../"))
         .stdout(Stdio::piped())
         .stderr(Stdio::piped())
@@ -56,6 +58,13 @@ static TEST_CTX: Lazy<TestCtx> = Lazy::new(|| {
     let image = GenericImage::new("artempikulin/esplora", "latest").with_wait_for(WaitFor::Log(
         LogWaitStrategy::stderr("Electrum RPC server running on"),
     ));
+
+    let host_mount_path = match env::var("GITHUB_ACTIONS") {
+        Ok(_) => {
+            "/home/runner/work/btc-lightclient/btc-lightclient/block_relayer/for_tests".to_string()
+        }
+        Err(_) => current_dir.join("for_tests").display().to_string(),
+    };
 
     let container_req = ContainerRequest::from(image)
         .with_cmd([
@@ -70,10 +79,7 @@ static TEST_CTX: Lazy<TestCtx> = Lazy::new(|| {
         .with_mapped_port(50001, 50001.tcp())
         .with_mapped_port(8094, 80.tcp())
         .with_mapped_port(18443, 18443.tcp())
-        .with_mount(Mount::bind_mount(
-            current_dir.join("for_tests").display().to_string(),
-            "/data",
-        ));
+        .with_mount(Mount::bind_mount(host_mount_path, "/data"));
 
     let esplora_container = TEST_RUNTIME
         .block_on(container_req.start())
@@ -93,13 +99,13 @@ static TEST_CTX: Lazy<TestCtx> = Lazy::new(|| {
 #[test]
 fn init_program() {
     let relay_config = RelayConfig {
-        bitcoind_url: "http://localhost:18443".into(),
+        bitcoind_url: "http://127.0.0.1:18443".into(),
         bitcoin_auth: BitcoinAuth::UserPass {
             user: "test".into(),
             password: "test".into(),
         },
-        yona_http: "http://localhost:8899".into(),
-        yona_ws: "ws://localhost:8900/".into(),
+        yona_http: "http://127.0.0.1:8899".into(),
+        yona_ws: "ws://127.0.0.1:8900/".into(),
         yona_keipair: TEST_CTX
             .current_dir
             .join("../anchor.json")
