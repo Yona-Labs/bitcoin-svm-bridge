e757672bab702f858cc9cc277120ad794f0672db
Artem Vitae <15745003+artemii235@users.noreply.github.com>
2025-06-10 19:55:21 +0700

    Add BridgeWithdraw command.

--- block_relayer/src/main.rs
+++ block_relayer/src/main.rs
@@ -1,3 +1,6 @@
+use anchor_client::solana_sdk::commitment_config::CommitmentConfig;
+use anchor_client::solana_sdk::signature::read_keypair_file;
+use anchor_client::{Client as AnchorClient, Cluster};
 use bitcoin::hashes::hash160::Hash as Hash160;
 use bitcoin::hashes::Hash;
 use bitcoin::hex::{DisplayHex, FromHex};
@@ -5,20 +8,30 @@ use bitcoin::key::{Keypair, Secp256k1};
 use bitcoin::secp256k1::SecretKey;
 use bitcoin::{Network, PrivateKey};
 use block_relayer_lib::config::read_config;
+use block_relayer_lib::relay_program_interaction::bridge_withdraw;
 use block_relayer_lib::{
     process_bridge_events, relay_blocks_from_full_node, relay_transactions, run_init_program,
 };
 use clap::{Parser, Subcommand};
 use sqlx::SqlitePool;
 use std::str::FromStr;
-use tokio::runtime;
+use std::sync::Arc;
 use tokio::runtime::Runtime;
 
 #[derive(Subcommand)]
 enum RelayerCommand {
-    InitProgram { deposit_pubkey: String },
-    Relay { bridge_privkey: String },
+    InitProgram {
+        deposit_pubkey: String,
+    },
+    Relay {
+        bridge_privkey: String,
+    },
     GenerateKey,
+    BridgeWithdraw {
+        bitcoin_address: String,
+        amount: u64,
+        yona_keypair: String,
+    },
 }
 
 #[derive(Parser)]
@@ -89,6 +102,24 @@ fn main() {
             let secp256k1 = Secp256k1::new();
             let keypair = Keypair::from_secret_key(&secp256k1, &key);
             println!("Public key {:02x}", keypair.public_key());
+        }
+        RelayerCommand::BridgeWithdraw {
+            bitcoin_address,
+            amount,
+            yona_keypair,
+        } => {
+            let yona_keypair = read_keypair_file(yona_keypair).expect("Failed to read keypair");
+            let signer = Arc::new(yona_keypair);
+            let cluster = Cluster::Custom(config.yona_http.clone(), config.yona_ws.clone());
+            let client =
+                AnchorClient::new_with_options(cluster, signer, CommitmentConfig::confirmed());
+            let program = client
+                .program(btc_relay::id())
+                .expect("Couldn't create relay program instance");
+
+            let result =
+                bridge_withdraw(&program, amount, bitcoin_address).expect("Successful withdrawal");
+            println!("Withdraw result {result}");
         }
     }
 }
